7072e042bafe5e3ff72f23e7440037c8438c8469
Chirandip Roy <chirandiproy@gmail.com>
2026-08-28 00:52:12 +0530

    Refactor attendance tracking logic and database queries

--- Attendace Tracker.py
+++ Attendace Tracker.py
@@ -20,18 +20,15 @@ print("""WELCOME TO THE ATTENDANCE REGISTER DATABASE
 
 cursor = conn.cursor()
 
-students = [ ]
-present = [ ]
-classes = [ ]
-cursor.execute("select * from attendancee")
+students = []
+
+cursor.execute("SELECT Roll FROM students ORDER BY Roll")
 data = cursor.fetchall()
-count = cursor.rowcount
-cursor.execute("select * from attendancee")
-for j in range(count):
-    data = cursor.fetchone()
-    students.append(data[0])
-    present.append(data[1])
-    classes.append(data[2])
+
+for row in data:
+    students.append(row[0])
+
+count = len(students)
 
 
 print("""What do you want to do?
@@ -41,75 +38,149 @@ print("""What do you want to do?
       4. Check the entire attendance register till date""")
 CHOICE = int(input("Enter your choice(1,2,3,4): "))
 
-if CHOICE==1:
-    x = (datetime.datetime.now())
-    day = x.strftime("%d")
-    month = x.strftime("%m")
-    year = x.strftime("%y")
-    y = str(day)+"_"+str(month)+"_"+str(year)
-    prom = "ALTER TABLE attendancee ADD "+y+" integer "
-    cursor.execute(prom)
-    conn.commit()
+if CHOICE == 1:
 
-    for i in range(count):
-        print("Is ",students[i]," present? ")
-        ans = input("(Y/N): ")
-        
-        if ans=="Y":
-            cmd = "UPDATE attendancee SET "+y+" = 1 WHERE Roll = "+str(students[i])
-            cursor.execute(cmd)
-            conn.commit()
-            cmd = "UPDATE attendancee SET Total = "+str(present[i]+1)+ " WHERE Roll = "+str(students[i])
-            cursor.execute(cmd)
-            conn.commit()
-            cmd = "UPDATE attendancee SET Total_Classes = "+str(classes[i]+1)+ " WHERE Roll = "+str(students[i])
-            cursor.execute(cmd)
-            conn.commit()
-        
-        elif ans=="N":
-            cmd = "UPDATE attendancee SET "+y+" = 0 WHERE Roll = "+str(students[i])
-            cursor.execute(cmd)
-            conn.commit()
-            cmd = "UPDATE attendancee SET Total_Classes = "+str(classes[i]+1)+ " WHERE Roll = "+str(students[i])
-            cursor.execute(cmd)
-            conn.commit()
+    today = datetime.date.today()
 
-        else:
-            print("ERROR")
-        
-    #Collection of data and storing it in text files
-    da = "select * from attendancee"
-    sql_query = pd.read_sql_query(da,conn)
-    df = pd.DataFrame(sql_query)
+    print("\nEntering attendance for:", today.strftime("%d_%m_%Y"))
 
-    df.to_csv(r'a.csv', index=False)
-    dff = pd.read_csv('a.csv')
-    print(dff)
+    check_sql = """
+        SELECT COUNT(*)
+        FROM attendance
+        WHERE Date = %s
+    """
 
-elif CHOICE==2:
-    date = input("Enter the date in dd_mm_yy fromat : ")
-    cmd = "SELECT * FROM attendancee WHERE "+date+" = 1"
-    cursor.execute(cmd)
-    data1 = cursor.fetchall()
-    count1 = cursor.rowcount
-    print("Present : ",count1,"\nAbsent : ",count - count1)
+    cursor.execute(check_sql, (today,))
+    attendance_count = cursor.fetchone()[0]
 
-elif CHOICE==3:
-    roll = input("Enter the roll number to be searched for : ")
-    cmd = "select * from attendancee where Roll = "+roll
-    cursor.execute(cmd)
-    data1 = cursor.fetchall()
-    print("Total Working Days : ",data1[0][2],"\nTotal No. of Days Present : ",data1[0][1])
-    print("Attendance Percentage : ",(data1[0][1]/data1[0][2])*100,"%")
+    if attendance_count > 0:
 
-elif CHOICE==4:
-    da = "select * from attendancee"
-    sql_query = pd.read_sql_query(da,conn)
-    df = pd.DataFrame(sql_query)
+        print("\nERROR: Today's attendance has already been entered.")
+        print("Date:", today.strftime("%d_%m_%Y"))
+        print("Attendance records found:", attendance_count)
+        print("You cannot enter attendance again today.")
 
-    df.to_csv(r'a.csv', index=False)
-    dff = pd.read_csv('a.csv')
-    print(dff)
+    else:
+
+        for roll in students:
+
+            while True:
+                ans = input(f"Is {roll} present? (Y/N): ").upper()
+
+                if ans == "Y":
+                    status = 1
+                    break
+
+                elif ans == "N":
+                    status = 0
+                    break
+
+                else:
+                    print("Invalid input. Please enter Y or N.")
+
+            sql = """
+                INSERT INTO attendance (Date, Roll, Status)
+                VALUES (%s, %s, %s)
+            """
+
+            cursor.execute(sql, (today, roll, status))
+
+        conn.commit()
+
+        print("\nAttendance successfully recorded.")
+
+elif CHOICE == 2:
+
+    date_input = input("Enter the date (dd_mm_yyyy): ")
+
+    try:
+        date = datetime.datetime.strptime(
+            date_input,
+            "%d_%m_%Y"
+        ).date()
+
+    except ValueError:
+        print("Invalid date format.")
+        exit()
+
+    cursor.execute("""
+        SELECT Roll
+        FROM attendance
+        WHERE Date = %s
+        AND Status = 1
+        ORDER BY Roll
+    """, (date,))
+
+    present_students = cursor.fetchall()
+
+    cursor.execute("""
+        SELECT COUNT(*)
+        FROM attendance
+        WHERE Date = %s
+    """, (date,))
+
+    total = cursor.fetchone()[0]
+
+    print("\nDate:", date_input)
+    print("Present:", len(present_students))
+    print("Absent:", total - len(present_students))
+
+    print("\nPresent students:")
+
+    for student in present_students:
+        print(student[0])
+
+elif CHOICE == 3:
+
+    roll = input("Enter the roll number to be searched for: ")
+
+    cursor.execute("""
+        SELECT
+            COUNT(*) AS total_classes,
+            COALESCE(SUM(Status), 0) AS present
+        FROM attendance
+        WHERE Roll = %s
+    """, (roll,))
+
+    total_classes, present = cursor.fetchone()
+
+    if total_classes == 0:
+        print("No attendance records found.")
+    else:
+        percentage = (present / total_classes) * 100
+
+        print("\nRoll Number:", roll)
+        print("Total Working Days:", total_classes)
+        print("Total Days Present:", present)
+        print("Total Days Absent:", total_classes - present)
+        print("Attendance Percentage:", round(percentage, 2), "%")
+
+elif CHOICE == 4:
+
+    query = """
+        SELECT Date, Roll, Status
+        FROM attendance
+        ORDER BY Date, Roll
+    """
+
+    df = pd.read_sql_query(query, conn)
+
+    if df.empty:
+        print("No attendance records found.")
+    else:
+
+        register = df.pivot(
+            index="Date",
+            columns="Roll",
+            values="Status"
+        )
+
+        register.index = pd.to_datetime(register.index).strftime("%d_%m_%Y")
+
+        print("\nENTIRE ATTENDANCE REGISTER\n")
+        print(register.to_string())
+
+        register.to_csv("attendance_register.csv")
 
 else:
     print("Wrong Choice Entered")
